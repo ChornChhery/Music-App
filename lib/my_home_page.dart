c0995cb5ede41ae3226aa3a4eed60c5efd99d688
ChornChhery <chherychorn@gmail.com>
2025-08-13 16:56:00 +0700

    Tmr continue 42:00 on yb

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -10,9 +10,11 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
 
   List popularSongs;
+  ScrollController _scrollController;
+  TabController _tabController;
 
   ReadData() async{
     await DefaultAssetBundle.of(context).loadString("json/popularSongs.json").then((s) {
@@ -25,6 +27,8 @@ class _MyHomePageState extends State<MyHomePage> {
   @override 
   void initState() {
     super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _scrollController = ScrollController();
     ReadData();
   }
 
@@ -100,7 +104,61 @@ class _MyHomePageState extends State<MyHomePage> {
                   )
                   ]
                 ),
-              )
+              ),
+
+              Expanded(child: NestedScrollView(
+                controller: _scrollController,
+                headerSliverBuilder: (BuildContext context, bool inScroll){
+                  return [
+                    SliverAppBar(
+                      pinned: true,
+                      bottom: PreferredSize(
+                        preferredSize: Size.fromHeight(50),
+                        child: Container(
+                          margin: const EdgeInsets.all(0),
+                          child: TabBar(
+                            indicatorPadding: const EdgeInsets.all(0),
+                            indicatorSize: TabBarIndicatorSize.label,
+                            labelPadding: const EdgeInsets.all(0),
+                            controller: _tabController,
+                            isScrollable: true,
+                            indicator: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.2),
+                                  blurRadius: 7,
+                                  offset: Offset(0, 0)
+                                )
+                              ]
+                            ),
+                            tabs: [
+                              Container(
+                                width: 120,
+                                height: 50,
+                                child: Text(
+                                  "New", style: TextStyle(color: Colors.white),
+
+                                ),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.3),
+                                      blurRadius: 7,
+                                      offset: Offset(0, 0)
+                                    )
+                                  ]
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ];
+                },
+              ))
             ],
           ),
         ),
