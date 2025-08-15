4277faf39d96fc19f519b922db207064697c4ad8
ChornChhery <chherychorn@gmail.com>
2025-08-15 19:11:10 +0700

    Fix bug of home page

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/cupertino.dart';
-import 'app_colors.dart' as AppColors;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
+import 'app_colors.dart' as AppColors;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -11,28 +11,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late List<dynamic> popularSongs;
+  late ScrollController _scrollController;
+  late TabController _tabController;
 
-  List popularSongs;
-  ScrollController _scrollController;
-  TabController _tabController;
-
-  ReadData() async{
-    await DefaultAssetBundle.of(context).loadString("json/popularSongs.json").then((s) {
-      setState(() {
-        popularSongs = json.decode(s);
-      });
-    });
-  }
-
-  @override 
+  @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 3, vsync: this);
+    popularSongs = [];
     _scrollController = ScrollController();
-    ReadData();
+    _tabController = TabController(length: 3, vsync: this);
+    _loadSongs();
   }
 
+  Future<void> _loadSongs() async {
+    try {
+      final String jsonString = await rootBundle.loadString('json/popularSongs.json');
+      final List<dynamic> data = json.decode(jsonString);
 
+      // Trim whitespace from URLs
+      data.forEach((song) {
+        song['image'] = (song['image'] as String).trim();
+        song['audio'] = (song['audio'] as String).trim();
+      });
+
+      setState(() {
+        popularSongs = data;
+      });
+    } catch (e) {
+      print("Failed to load songs: $e");
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,203 +58,154 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         child: Scaffold(
           body: Column(
             children: [
-              Container(
-                margin: const EdgeInsets.only(left: 20, right: 20),
-                child: 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(Icons.menu, size: 24, color: Colors.black),
-                      Row(
-                        children: [
-                          Icon(Icons.search),
-                          SizedBox(width: 10,),
-                          Icon(Icons.notifications)
-                        ],
-                      ),
-                    ],
-                  ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(left: 20),
-                    child: Text("Music App", style: TextStyle(fontSize: 30)),
-
-                  )
-                ],
-              ),
-
-              SizedBox(height: 20,),
-              Container(
-                height: 180,
-                child: Stack(
+              // Header
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-
-                    Positioned(
-                      top: 0,
-                      left: -20,
-                      right: 0,
-
-                    child: Container(
-                      height: 180,
-                      child: PageView.builder(
-                          controller: PageController(viewportFraction: 0.8),
-                          itemCount: popularSongs==null?0:popularSongs.length,
-                          itemBuilder: (_, i){
-                        return Container(
-                          height: 180,
-                          width: MediaQuery.of(context).size.width,
-                          margin: const EdgeInsets.only(right: 10),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            image: DecorationImage(
-                              image: NetworkImage(popularSongs[i]["image"]),
-                              fit: BoxFit.fill,
-                            )
-                          ),
-                        );
-                      }),
-                    )
-                  )
-                  ]
-                ),
-              ),
-
-              Expanded(child: NestedScrollView(
-                controller: _scrollController,
-                headerSliverBuilder: (BuildContext context, bool inScroll){
-                  return [
-                    SliverAppBar(
-                      pinned: true,
-                      backgroundColor: AppColors.sliverBackground,
-                      bottom: PreferredSize(
-                        preferredSize: Size.fromHeight(50),
-                        child: Container(
-                          margin: const EdgeInsets.only(bottom: 20, left: 10),
-                          child: TabBar(
-                            indicatorPadding: const EdgeInsets.all(0),
-                            indicatorSize: TabBarIndicatorSize.label,
-                            labelPadding: const EdgeInsets.only(right: 10),
-                            controller: _tabController,
-                            isScrollable: true,
-                            indicator: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.2),
-                                  blurRadius: 7,
-                                  offset: Offset(0, 0)
-                                )
-                              ]
-                            ),
-                            tabs: [
-                              Container(
-                                width: 120,
-                                height: 50,
-                                child: Text(
-                                  "New", style: TextStyle(color: Colors.white),
-
-                                ),
-                                alignment: Alignment.center,
-
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: AppColors.menu1Color,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.3),
-                                      blurRadius: 7,
-                                      offset: Offset(0, 0)
-                                    )
-                                  ]
-                                ),
-                              ),
-                                                            Container(
-                                width: 120,
-                                height: 50,
-                                child: Text(
-                                  "New", style: TextStyle(color: Colors.white),
-
-                                ),
-                                alignment: Alignment.center,
-
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: AppColors.menu2Color,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.3),
-                                      blurRadius: 7,
-                                      offset: Offset(0, 0)
-                                    )
-                                  ]
-                                ),
-                              ),
-                              Container(
-                                width: 120,
-                                height: 50,
-                                child: Text(
-                                  "New", style: TextStyle(color: Colors.white),
-
-                                ),
-                                alignment: Alignment.center,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  color: AppColors.menu3Color,
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.3),
-                                      blurRadius: 7,
-                                      offset: Offset(0, 0)
-                                    )
-                                  ]
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    )
-                  ];
-                },
-                body: TabBarView(
-                  children: [
-                    Material(
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.grey,
-                        ),
-                        title: Text("Content")
-                      ),
-                      
-                    ),
-                    Material(
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.grey,
-                        ),
-                        title: Text("Content")
-                      ),
-                      
-                    ),
-                    Material(
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.grey,
-                        ),
-                        title: Text("Content")
-                      ),
-                      
+                    const Icon(Icons.menu, size: 24, color: Colors.black),
+                    Row(
+                      children: const [
+                        Icon(Icons.search),
+                        SizedBox(width: 10),
+                        Icon(Icons.notifications),
+                      ],
                     ),
                   ],
                 ),
-              ))
+              ),
+
+              // Title
+              Padding(
+                padding: const EdgeInsets.only(left: 20, bottom: 20),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Music App",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+
+              // Image Carousel (PageView)
+              SizedBox(
+                height: 180,
+                child: PageView.builder(
+                  controller: PageController(viewportFraction: 0.85),
+                  itemCount: popularSongs.length,
+                  itemBuilder: (context, index) {
+                    final song = popularSongs[index];
+                    return Container(
+                      margin: const EdgeInsets.only(right: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: DecorationImage(
+                          image: NetworkImage(song['image']),
+                          fit: BoxFit.cover,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            blurRadius: 10,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              // TabBar with NestedScrollView
+              Expanded(
+                child: NestedScrollView(
+                  controller: _scrollController,
+                  headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                    SliverAppBar(
+                      pinned: true,
+                      backgroundColor: AppColors.sliverBackground,
+                      bottom: TabBar(
+                        controller: _tabController,
+                        isScrollable: true,
+                        indicator: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              blurRadius: 7,
+                              offset: Offset(0, 0),
+                            ),
+                          ],
+                        ),
+                        tabs: [
+                          _buildTab("New", AppColors.menu1Color),
+                          _buildTab("Popular", AppColors.menu2Color),
+                          _buildTab("Trending", AppColors.menu3Color),
+                        ],
+                      ),
+                    ),
+                  ],
+                  body: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      _buildSongList(),
+                      _buildSongList(),
+                      _buildSongList(),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  // Helper: Build a styled tab
+  Widget _buildTab(String label, Color color) {
+    return Container(
+      width: 120,
+      height: 50,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Text(
+        label,
+        style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+      ),
+    );
+  }
+
+  // Helper: Build list of songs
+  Widget _buildSongList() {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      itemCount: popularSongs.length,
+      itemBuilder: (context, index) {
+        final song = popularSongs[index];
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(song['image']),
+            backgroundColor: Colors.grey[300],
+          ),
+          title: Text(song['title'], style: const TextStyle(fontWeight: FontWeight.bold)),
+          subtitle: Text(song['text'], style: TextStyle(color: Colors.grey[600])),
+          trailing:  Icon(Icons.play_circle_fill, color:AppColors.loveColor, size: 30),
+        );
+      },
     );
   }
 }
